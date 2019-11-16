9d56ffc530392481516f4e89ce0029dec1d30ec1
本間喜明 <yoshiaki@honmayoshiakinoMacBook-Pro.local>
2019-11-17 05:00:11 +0900

    debug

--- models/retinanet.py
+++ models/retinanet.py
@@ -98,10 +98,11 @@ class RetinaNet(Chain):
                 _anchors.append(self.xp.array(
                     anchor[:, np.newaxis, :], dtype=np.float32))
             # anchors.append(self.xp.vstack(_anchors))
-            anchors.append(self.xp.concatenate(_anchors, axis=-2))
-        # anchors = self.xp.vstack(anchors)
-        anchors = self.xp.concatenate(anchors, axis=-2)
-        anchors = anchors.reshape(-1, 4)
+            anchors.append(self.xp.concatenate(
+                _anchors, axis=-2).reshape((-1, 4)))
+        anchors = self.xp.vstack(anchors)
+        # anchors = self.xp.concatenate(anchors, axis=-2)
+        # anchors = anchors.reshape(-1, 4)
         return anchors
 
     def _prepare(self, imgs):

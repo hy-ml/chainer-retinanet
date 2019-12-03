836f8f4d8fa12ca8e500a254e2a0e3c2b2d73e2d
hy-ws <h-y-bvb-united@outlook.jp>
2019-12-03 19:44:24 +0900

    remove unnecessary code

--- models/retinanet.py
+++ models/retinanet.py
@@ -101,13 +101,10 @@ class RetinaNet(Chain):
                 anchor[:, 2:] += anchor[:, :2]
                 _anchors.append(np.array(
                     anchor[:, np.newaxis, :], dtype=np.float32))
-            # anchors.append(self.xp.vstack(_anchors))
             anchors.append(np.concatenate(
                 _anchors, axis=-2).reshape((-1, 4)))
         anchors = np.vstack(anchors)
         anchors = self.xp.array(anchors)
-        # anchors = self.xp.concatenate(anchors, axis=-2)
-        # anchors = anchors.reshape(-1, 4)
         return anchors
 
     def _prepare(self, imgs):
